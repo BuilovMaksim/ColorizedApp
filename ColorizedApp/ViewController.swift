295cf2799f11de89bc8af1296a5df73d509e2a0a
Maksim Builov <builov.max.w@gmail.com>
2023-03-25 18:53:31 +0400

    Setting the initial values of the start view. Creating the application logic.

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -8,12 +8,63 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARC: - IB Outlets
+    @IBOutlet var mainView: UIView!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var counterValueRedSliderLabel: UILabel!
+    @IBOutlet var counterValueGreenSliderLabel: UILabel!
+    @IBOutlet var counterValueBlueSliderLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupStartVeiw()
     }
-
-
+    
+    // MARC: IB Actions
+    @IBAction func redSliderAction() {
+        counterValueRedSliderLabel.text = settingUpLabel(redSlider.value)
+        updatingBackgroundMainVeiw()
+    }
+    
+    @IBAction func greenSliderAction() {
+        counterValueGreenSliderLabel.text = settingUpLabel(greenSlider.value)
+        updatingBackgroundMainVeiw()
+    }
+    
+    @IBAction func blueSliderAction() {
+        counterValueBlueSliderLabel.text = settingUpLabel(blueSlider.value)
+        updatingBackgroundMainVeiw()
+    }
+    
+    // MARC: - Private Methods
+    private func setupStartVeiw () {
+        mainView.backgroundColor = .init(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+        counterValueRedSliderLabel.text = settingUpLabel(redSlider.value)
+        counterValueGreenSliderLabel.text = settingUpLabel(greenSlider.value)
+        counterValueBlueSliderLabel.text = settingUpLabel(blueSlider.value)
+    }
+    
+    private func settingUpLabel(_ value: Float) -> String {
+        String(format: "%.2f", value)
+    }
+    
+    private func updatingBackgroundMainVeiw() {
+        mainView.backgroundColor = .init(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
 }
 
